a8be8e2cbbf8422bb6a929572814d52f57877810
Ezra <ezraholm50@gmail.com>
2016-05-04 20:07:35 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ ADDRESS=$($IP route get 1 | awk '{print $NF;exit}')
 NETMASK=$($IFCONFIG $IFACE | grep Mask | sed s/^.*Mask://)
 GATEWAY=$($IP route | awk '/default/ { print $3 }')
 SCRIPTS="/var/scripts"
+REPO="https://github.com/ezraholm50/MediaCenter"
 
 # Check if root
         if [ "$(whoami)" != "root" ]; then
@@ -130,11 +131,110 @@ function ask_yes_or_no() {
 if [[ "yes" == $(ask_yes_or_no "Do you want to install a real certificate from Letsencrypt to secure your WAN access to the server.?") ]]
 then
 	wget $REPO/letsencrypt.sh -P $SCRIPTS/
-	bash $SCRIPTS/letsencrypt.sh
-else
-sleep 1
+	cat << STARTMSG
++---------------------------------------------------------------+
+|       Important! Please read this!                            |
+|                                                               |
+|       This script will install SSL from Let's Encrypt.        |
+|       It's free of charge, and very easy to use.              |
+|                                                               |
+|       Before we begin the installation you need to have       |
+|       a domain that the SSL certs will be valid for.          |
+|       If you don't have a domian yet, get one before          |
+|       you run this script!                                    |
+|								|
+|       You also have to open port 443 against this machine     |
+|       IP address: $ADDRESS - do this in your router.  |
+|       Here is a guide: https://goo.gl/Uyuf65                  |
+|                                                               |
+|       This script is located in /var/scripts and you          |
+|       can run this script after you got a domain.             |
+|                                                               |
+|       Please don't run this script if you don't have		|
+|       a domain yet. You can get one for a fair price here:	|
+|       https://www.citysites.eu/                               |
+|                                                               |
++---------------------------------------------------------------+
+
+STARTMSG
+
+	function ask_yes_or_no() {
+    	read -p "$1 ([y]es or [N]o): "
+    	case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+        y|yes) echo "yes" ;;
+        *)     echo "no" ;;
+    esac
+}
+if [[ "no" == $(ask_yes_or_no "Are you sure you want to continue?") ]]
+then
+	echo
+    	echo "OK, but if you want to run this script later, just type: sudo bash /var/scripts/letsencrypt.sh"
+    	echo -e "\e[32m"
+    	read -p "Press any key to continue... " -n1 -s
+    	echo -e "\e[0m"
+exit
 fi
-exit 0
+
+        function ask_yes_or_no() {
+        read -p "$1 ([y]es or [N]o): "
+        case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+        y|yes) echo "yes" ;;
+        *)     echo "no" ;;
+    esac
+}
+if [[ "no" == $(ask_yes_or_no "Have you forwarded port 443 in your router?") ]]
+then
+        echo
+        echo "OK, but if you want to run this script later, just type: sudo bash /var/scripts/letsencrypt.sh"
+        echo -e "\e[32m"
+        read -p "Press any key to continue... " -n1 -s
+        echo -e "\e[0m"
+exit
+fi
+
+    	function ask_yes_or_no() {
+    	read -p "$1 ([y]es or [N]o): "
+    	case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+        y|yes) echo "yes" ;;
+	*)     echo "no" ;;
+	esac
+}
+if [[ "yes" == $(ask_yes_or_no "Do you have a domian that you will use?") ]]
+then
+        sleep 1
+else
+	echo
+    	echo "OK, but if you want to run this script later, just type: sudo bash /var/scripts/letsencrypt.sh"
+    	echo -e "\e[32m"
+    	read -p "Press any key to continue... " -n1 -s
+    	echo -e "\e[0m"
+exit
+fi
+
+# Install git
+	git --version 2>&1 >/dev/null
+	GIT_IS_AVAILABLE=$?
+# ...
+	if [ $GIT_IS_AVAILABLE -eq 1 ]; then
+        sleep 1
+else
+        apt-get install git -y -q
+fi
+
+	cd /var
+	git clone https://github.com/letsencrypt/letsencrypt
+	cd letsencrypt
+	./letsencrypt-auto certonly --agree-tos
+	cd
+else
+	echo
+    	echo "OK, but if you want to run this script later, just type: sudo bash /var/scripts/letsencrypt.sh"
+    	echo -e "\e[32m"
+    	read -p "Press any key to continue... " -n1 -s
+    	echo -e "\e[0m"
+    	wget $REPO/letsencrypt.sh -P $SCRIPTS/
+exit
+fi
 
 # DynDns
 function ask_yes_or_no() {
